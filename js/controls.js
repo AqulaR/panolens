decfbd71f68a7e9533755a84df25acc47c303355
AqulaR <103633654+AqulaR@users.noreply.github.com>
2023-06-28 12:25:57 +0300

    Add files via upload

---
+++ js/controls.js
@@ -0,0 +1,175 @@
+// Колизей пано подключение
+const rim1 = new PANOLENS.ImagePanorama('assets/Colosseum/rim1.jpg');
+const rim2 = new PANOLENS.ImagePanorama('assets/Colosseum/rim2.jpg');
+const rim3 = new PANOLENS.ImagePanorama('assets/Colosseum/rim3.jpg');
+const rim4 = new PANOLENS.ImagePanorama('assets/Colosseum/rim4.jpg');
+const rim5 = new PANOLENS.ImagePanorama('assets/Colosseum/rim5.jpg');
+const rim6 = new PANOLENS.ImagePanorama('assets/Colosseum/rim6.jpg');
+const rim7 = new PANOLENS.ImagePanorama('assets/Colosseum/rim7.jpg');
+const rim8 = new PANOLENS.ImagePanorama('assets/Colosseum/rim8.jpg');
+const rim9 = new PANOLENS.ImagePanorama('assets/Colosseum/rim9.jpg');
+// Хансе пано подключение
+// const el_hasne1 = new PANOLENS.ImagePanorama('assets/el-hasne/el-hasne1.jpg');
+// const el_hasne2 = new PANOLENS.ImagePanorama('assets/el-hasne/el-hasne2.jpg');
+// const el_hasne3 = new PANOLENS.ImagePanorama('assets/el-hasne/el-hasne3.jpg');
+// const el_hasne4 = new PANOLENS.ImagePanorama('assets/el-hasne/el-hasne4.jpg');
+// const el_hasne5 = new PANOLENS.ImagePanorama('assets/el-hasne/el-hasne5.jpg');
+// const el_hasne6 = new PANOLENS.ImagePanorama('assets/el-hasne/el-hasne6.jpg');
+// const el_hasne7 = new PANOLENS.ImagePanorama('assets/el-hasne/el-hasne7.jpg');
+// const el_hasne8 = new PANOLENS.ImagePanorama('assets/el-hasne/el-hasne8.jpg');
+// const el_hasne9 = new PANOLENS.ImagePanorama('assets/el-hasne/el-hasne9.jpg');
+// // Тадж махал пано подключение
+// const tadzh_mah1 = new PANOLENS.ImagePanorama('assets/tadzh-mah/tadzh-mah1.jpg');
+// const tadzh_mah2 = new PANOLENS.ImagePanorama('assets/tadzh-mah/tadzh-mah2.jpg');
+// const tadzh_mah3 = new PANOLENS.ImagePanorama('assets/tadzh-mah/tadzh-mah3.jpg');
+// const tadzh_mah4 = new PANOLENS.ImagePanorama('assets/tadzh-mah/tadzh-mah4.jpg');
+// const tadzh_mah5 = new PANOLENS.ImagePanorama('assets/tadzh-mah/tadzh-mah5.jpg');
+// const tadzh_mah6 = new PANOLENS.ImagePanorama('assets/tadzh-mah/tadzh-mah6.jpg');
+// const tadzh_mah7 = new PANOLENS.ImagePanorama('assets/tadzh-mah/tadzh-mah7.jpg');
+// const tadzh_mah8 = new PANOLENS.ImagePanorama('assets/tadzh-mah/tadzh-mah8.jpg');
+// const tadzh_mah9 = new PANOLENS.ImagePanorama('assets/tadzh-mah/tadzh-mah9.jpg');
+// const tadzh_mah10 = new PANOLENS.ImagePanorama('assets/tadzh-mah/tadzh-mah10.jpg');
+// const tadzh_mah11 = new PANOLENS.ImagePanorama('assets/tadzh-mah/tadzh-mah11.jpg');
+// const tadzh_mah12 = new PANOLENS.ImagePanorama('assets/tadzh-mah/tadzh-mah12.jpg');
+// const tadzh_mah13 = new PANOLENS.ImagePanorama('assets/tadzh-mah/tadzh-mah13.jpg');
+// const tadzh_mah14 = new PANOLENS.ImagePanorama('assets/tadzh-mah/tadzh-mah14.jpg');
+// const tadzh_mah15 = new PANOLENS.ImagePanorama('assets/tadzh-mah/tadzh-mah15.jpg');
+// const tadzh_mah16 = new PANOLENS.ImagePanorama('assets/tadzh-mah/tadzh-mah16.jpg');
+// const tadzh_mah17 = new PANOLENS.ImagePanorama('assets/tadzh-mah/tadzh-mah17.jpg');
+// const tadzh_mah18 = new PANOLENS.ImagePanorama('assets/tadzh-mah/tadzh-mah18.jpg');
+// const tadzh_mah19 = new PANOLENS.ImagePanorama('assets/tadzh-mah/tadzh-mah19.jpg');
+// const tadzh_mah20 = new PANOLENS.ImagePanorama('assets/tadzh-mah/tadzh-mah20.jpg');
+// // chichen-iza пано подключение 
+// const chichen_iza1 = new PANOLENS.ImagePanorama('assets/chichen-iza/chichen-iza1.jpg');
+// const chichen_iza2 = new PANOLENS.ImagePanorama('assets/chichen-iza/chichen-iza2.jpg');
+// const chichen_iza3 = new PANOLENS.ImagePanorama('assets/chichen-iza/chichen-iza3.jpg');
+// const chichen_iza4 = new PANOLENS.ImagePanorama('assets/chichen-iza/chichen-iza4.jpg');
+// const chichen_iza5 = new PANOLENS.ImagePanorama('assets/chichen-iza/chichen-iza5.jpg');
+// const chichen_iza6 = new PANOLENS.ImagePanorama('assets/chichen-iza/chichen-iza6.jpg');
+// const chichen_iza7 = new PANOLENS.ImagePanorama('assets/chichen-iza/chichen-iza7.jpg');
+// const chichen_iza8 = new PANOLENS.ImagePanorama('assets/chichen-iza/chichen-iza8.jpg');
+// const chichen_iza9 = new PANOLENS.ImagePanorama('assets/chichen-iza/chichen-iza9.jpg');
+// const chichen_iza10 = new PANOLENS.ImagePanorama('assets/chichen-iza/chichen-iza10.jpg');
+// const chichen_iza11 = new PANOLENS.ImagePanorama('assets/chichen-iza/chichen-iza11.jpg');
+// // jesus-isk пано подключение 
+// const jesus_isk1 = new PANOLENS.ImagePanorama('assets/jesus-isk/jesus-isk1.jpg');
+// const jesus_isk2 = new PANOLENS.ImagePanorama('assets/jesus-isk/jesus-isk2.jpg');
+// const jesus_isk3 = new PANOLENS.ImagePanorama('assets/jesus-isk/jesus-isk3.jpg');
+// const jesus_isk4 = new PANOLENS.ImagePanorama('assets/jesus-isk/jesus-isk4.jpg');
+// const jesus_isk5 = new PANOLENS.ImagePanorama('assets/jesus-isk/jesus-isk5.jpg');
+// const jesus_isk6 = new PANOLENS.ImagePanorama('assets/jesus-isk/jesus-isk6.jpg');
+// const jesus_isk7 = new PANOLENS.ImagePanorama('assets/jesus-isk/jesus-isk7.jpg');
+// const jesus_isk8 = new PANOLENS.ImagePanorama('assets/jesus-isk/jesus-isk8.jpg');
+// // machu-pickchu пано подключение 
+// const machu_pickchu1 = new PANOLENS.ImagePanorama('assets/machu-pickchu/machu-pickchu1.jpg');
+// const machu_pickchu2 = new PANOLENS.ImagePanorama('assets/machu-pickchu/machu-pickchu2.jpg');
+// const machu_pickchu3 = new PANOLENS.ImagePanorama('assets/machu-pickchu/machu-pickchu3.jpg');
+// const machu_pickchu4 = new PANOLENS.ImagePanorama('assets/machu-pickchu/machu-pickchu4.jpg');
+// const machu_pickchu5 = new PANOLENS.ImagePanorama('assets/machu-pickchu/machu-pickchu5.jpg');
+// const machu_pickchu6 = new PANOLENS.ImagePanorama('assets/machu-pickchu/machu-pickchu6.jpg');
+// const machu_pickchu7 = new PANOLENS.ImagePanorama('assets/machu-pickchu/machu-pickchu7.jpg');
+// const machu_pickchu8 = new PANOLENS.ImagePanorama('assets/machu-pickchu/machu-pickchu8.jpg');
+// const machu_pickchu9 = new PANOLENS.ImagePanorama('assets/machu-pickchu/machu-pickchu9.jpg');
+// const machu_pickchu10 = new PANOLENS.ImagePanorama('assets/machu-pickchu/machu-pickchu10.jpg');
+// const machu_pickchu11 = new PANOLENS.ImagePanorama('assets/machu-pickchu/machu-pickchu11.jpg');
+// const machu_pickchu12 = new PANOLENS.ImagePanorama('assets/machu-pickchu/machu-pickchu12.jpg');
+// const machu_pickchu13 = new PANOLENS.ImagePanorama('assets/machu-pickchu/machu-pickchu13.jpg');
+// const machu_pickchu14 = new PANOLENS.ImagePanorama('assets/machu-pickchu/machu-pickchu14.jpg');
+// const machu_pickchu15 = new PANOLENS.ImagePanorama('assets/machu-pickchu/machu-pickchu15.jpg');
+// // kitai-wall пано подключение 
+// const kitai_wall1 = new PANOLENS.ImagePanorama('assets/kitai-wall/kitai-wall1.jpg');
+// const kitai_wall2 = new PANOLENS.ImagePanorama('assets/kitai-wall/kitai-wall2.jpg');
+// const kitai_wall3 = new PANOLENS.ImagePanorama('assets/kitai-wall/kitai-wall3.jpg');
+// const kitai_wall4 = new PANOLENS.ImagePanorama('assets/kitai-wall/kitai-wall4.jpg');
+
+let imageContainer = document.querySelector('.panorama__container');
+
+let SpotPositions = [
+    // На небеса.exe
+    new THREE.Vector3(0, 5000, 0),
+]
+
+let lookAtPositions = [
+    new THREE.Vector3(-706, 0, 200),
+    new THREE.Vector3(-2360, -1390, -4180 ),
+    new THREE.Vector3(-4350, -2460, -26 ),
+    new THREE.Vector3(3609, -2921, -1830 )
+];
+
+let Colosseum_SpotPos = [
+    //1
+    new THREE.Vector3(-4800, -700, 2000),
+    //2
+    new THREE.Vector3(1750, 0, 100),
+    new THREE.Vector3(-4800, -700, 2000),
+    //3
+    new THREE.Vector3(1000, 0, 3000),
+    new THREE.Vector3(-1800, 0, 400),
+    //4
+    new THREE.Vector3(-2600, 0, 1200),
+    new THREE.Vector3(1800, -200, -1000),
+    // 5
+    new THREE.Vector3(3000, 1200, -1300),
+    new THREE.Vector3(-1800, 0, 400),
+    new THREE.Vector3(-1700, -700, 700),
+    new THREE.Vector3(1500, 400, -2300),
+    new THREE.Vector3(1700, 1000, 1300),
+    // 6
+    new THREE.Vector3(2000, -1500, 0),
+    // 7
+    new THREE.Vector3(3000, 100, -1300),
+    new THREE.Vector3(-2800, 400, -1300),
+    new THREE.Vector3(0, 400, -3000),
+    // 8
+    new THREE.Vector3(-2500, -400, -100),
+    new THREE.Vector3(-2500, -700, -1100),
+    new THREE.Vector3(-2200, -300, 1800),
+    // 9
+    new THREE.Vector3(2000, -500, -1800),
+    new THREE.Vector3(2000, -100, 1800),
+    new THREE.Vector3(2000, -300, 0),
+]
+
+const viewer = new PANOLENS.Viewer({
+    container: imageContainer,
+    cameraFov: 80
+});
+
+// Связь в коридоре
+rim1.link(rim1, SpotPositions[0]);
+rim1.link(rim2, Colosseum_SpotPos[0]);
+
+rim2.link(rim1, Colosseum_SpotPos[1]);
+rim2.link(rim4, Colosseum_SpotPos[2]);
+
+rim4.link(rim5, Colosseum_SpotPos[3]);
+rim4.link(rim2, Colosseum_SpotPos[4]);
+
+rim5.link(rim4, Colosseum_SpotPos[5]);
+rim5.link(rim7, Colosseum_SpotPos[6]);
+
+rim7.link(rim6, Colosseum_SpotPos[7]);
+rim7.link(rim3, Colosseum_SpotPos[8]);
+rim7.link(rim5, Colosseum_SpotPos[9]);
+rim7.link(rim8, Colosseum_SpotPos[10]);
+rim7.link(rim9, Colosseum_SpotPos[11]);
+
+rim3.link(rim7, Colosseum_SpotPos[12]);
+
+rim8.link(rim7, Colosseum_SpotPos[13]);
+rim8.link(rim6, Colosseum_SpotPos[14]);
+rim8.link(rim9, Colosseum_SpotPos[15]);
+
+rim6.link(rim7, Colosseum_SpotPos[16]);
+rim6.link(rim8, Colosseum_SpotPos[17]);
+rim6.link(rim9, Colosseum_SpotPos[18]);
+
+rim9.link(rim7, Colosseum_SpotPos[19]);
+rim9.link(rim6, Colosseum_SpotPos[20]);
+rim9.link(rim8, Colosseum_SpotPos[21]);
+
+viewer.add(rim1, rim2, rim3, rim4, rim5, rim6, rim7, rim8, rim9);
+
+rim1.addEventListener( 'enter-fade-start', function(){
+    viewer.tweenControlCenter( lookAtPositions[0], 1000 );
+} );
